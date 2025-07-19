6c674ee04d4b1d8a54f27e7f897800ea8e6ac65a
Hamid Dehnavi <hamid.dev.pro@gmail.com>
2025-07-20 02:38:37 +0330

    feat: allow custom notification channels

--- src/Notifications/Notifiable.php
+++ src/Notifications/Notifiable.php
@@ -3,10 +3,30 @@
 namespace Spatie\Health\Notifications;
 
 use Illuminate\Notifications\Notifiable as NotifiableTrait;
+use Illuminate\Support\Str;
 
 class Notifiable
 {
     use NotifiableTrait;
+
+    public function routeNotificationFor(string $driver, mixed $notification = null): mixed
+    {
+        if (method_exists($this, $method = 'routeNotificationFor'.Str::studly($driver))) {
+            return $this->{$method}($notification);
+        }
+
+        $customChannels = config('health.notifications.custom_channels', []);
+
+        if (array_key_exists($driver, $customChannels)) {
+            return $customChannels[$driver];
+        }
+
+        return match ($driver) {
+            'database' => $this->notifications(),
+            'mail' => $this->routeNotificationForMail(),
+            default => null,
+        };
+    }
 
     /** @return string|array<int, string> */
     public function routeNotificationForMail(): string|array
